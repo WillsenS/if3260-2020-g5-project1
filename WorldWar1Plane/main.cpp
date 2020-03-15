6929339fb839a464c463d823cd6887f6b79ff63e
Suhailie <13517045@std.stei.itb.ac.id>
2020-03-15 17:31:11 +0700

    add fitur kamera mengitari model

--- WorldWar1Plane/main.cpp
+++ WorldWar1Plane/main.cpp
@@ -7,9 +7,17 @@
 #include <math.h>
 #include <stdlib.h>
 
-static float rotXZ = 0.0;
-static float rotXY = 0.0;
-static float zoom = 0.0;
+static float rotCamXY = 0.0;//kamera mengitari model
+static float rotCamPro = 0.0; //memutar bidang proyeksi kamera
+static float zoom = 0.0; //radius
+
+static float rotX = 0.0;
+static float rotY = 0.0;
+static float rotZ = 0.0;
+
+static float refX = 0.0;
+static float refY = 0.0;
+static float refZ = 0.0;
 
 static float posX = 0.0;
 static float posY = 0.0;
@@ -20,36 +28,33 @@ static float viewY = 0.0;
 static float viewZ = 0.0;
 
 static float upX = 0.0;
-static float upY = 1.0;
-static float upZ = 0.0;
-
-static float rotX = 0.0;
-static float rotY = 0.0;
-static float rotZ = 0.0;
+static float upY = 0.0;
+static float upZ = 1.0;
 
 static void init(void)
 {
-    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClearColor(0,0,0,0);
     glShadeModel(GL_FLAT);
+    glEnable(GL_DEPTH_TEST);
 }
 
 static void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glColor3ub(255, 255, 255);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    glTranslatef(0.0f, 0.0f, -7.0f);
+    glTranslatef(0.0f, 0.0f, -7.0f + zoom);
 
-    // For rotation
     glRotatef(0.0+rotX, 1.0, 0.0, 0.0);
     glRotatef(0.0+rotY, 0.0, 1.0, 0.0);
     glRotatef(0.0+rotZ, 0.0, 0.0, 1.0);
 
     gluLookAt(posX, posY, posZ,
-              viewX + cos(rotXZ * M_PI / 180), viewY, viewZ + sin(rotXZ * M_PI / 180),
-              upX + cos(rotXY * M_PI / 180), upY + sin(rotXY * M_PI / 180), upZ);
+              viewX, viewY, viewZ,
+              upX + refX, upY + refY, upZ + refZ);
 
-    glScalef(1.0 + zoom, 1.0 + zoom, 1.0 + zoom);
+    glScalef(1.0, 1.0, 1.0);
 
     glBegin(GL_QUADS);
     glColor3f(0.0, 0.0, 1.0);
@@ -90,6 +95,49 @@ static void display(void)
 
     glEnd();
 
+    glRotatef(0.0+rotX, 1.0, 0.0, 0.0);
+    glRotatef(0.0+rotY, 0.0, 1.0, 0.0);
+    glRotatef(0.0+rotZ, 0.0, 0.0, 1.0);
+
+    glBegin(GL_QUADS);
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3f( 4.0, 4.0, 4.0);
+    glVertex3f( 4.0, 4.0, 2.0);
+    glVertex3f( 2.0, 4.0, 2.0);
+    glVertex3f( 2.0, 4.0, 4.0);
+
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3f( 2.0, 2.0, 4.0);
+    glVertex3f( 2.0, 2.0, 2.0);
+    glVertex3f( 4.0, 2.0, 2.0);
+    glVertex3f( 4.0, 2.0, 4.0);
+
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f( 2.0, 4.0, 4.0);
+    glVertex3f( 2.0, 2.0, 4.0);
+    glVertex3f( 4.0, 2.0, 4.0);
+    glVertex3f( 4.0, 4.0, 4.0);
+
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f( 2.0, 4.0, 2.0);
+    glVertex3f( 4.0, 4.0, 2.0);
+    glVertex3f( 4.0, 2.0, 2.0);
+    glVertex3f( 2.0, 2.0, 2.0);
+
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex3f( 2.0, 4.0, 4.0);
+    glVertex3f( 2.0, 4.0, 2.0);
+    glVertex3f( 2.0, 2.0, 2.0);
+    glVertex3f( 2.0, 2.0, 4.0);
+
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex3f( 4.0, 4.0, 4.0);
+    glVertex3f( 4.0, 2.0, 4.0);
+    glVertex3f( 4.0, 2.0, 2.0);
+    glVertex3f( 4.0, 4.0, 2.0);
+
+    glEnd();
+
     glFlush();
     glutSwapBuffers();
 }
@@ -101,19 +149,26 @@ static void key(unsigned char key, int x, int y)
         case 'q':
             exit(0);
             break;
-        case 'w':
-            rotXY+=5;
-            upY = 0;
-            break;
         case 's':
-            rotXY-=5;
-            upY = 0;
+            //posX = (zoom + view)
             break;
         case 'a':
-            rotXZ+=5;
             break;
         case 'd':
-            rotXZ-=5;
+            break;
+        case 'w':
+            rotCamXY+=3;
+            posX = cos(M_PI * rotXY / 180);
+            posY = sin(M_PI * rotXY / 180);
+            break;
+        case 'x':
+            rotX+=3;
+            break;
+        case 'y':
+            rotY+=3;
+            break;
+        case 'z':
+            rotZ+=3;
             break;
         case 'o':
             zoom+=0.1;
@@ -121,35 +176,25 @@ static void key(unsigned char key, int x, int y)
         case 'p':
             zoom-=0.1;
             break;
-        case 'x':
-            rotX += 5;
-            break;
-        case 'y':
-            rotX += 5;
-            break;
-        case 'z':
-            rotX += 5;
-            break;
         case 'r':
-            rotXZ = 0;
-            rotXY = 0;
-            rotX = 0;
-            rotY = 0;
-            rotZ = 0;
+            rotXZ = 0, rotXY = 0, rotYZ = 0;
+            rotX = 0, rotY = 0, rotZ = 0;
+            viewX = 0, viewY = 0, viewZ = 0;
+            posX = 0, posY = 0, posZ = 0;
+            upX = 0, upY = 0, upZ = 1;
+            refX = 0, refY = 0, refZ = 0;
             zoom = 0;
-            upY = 1;
             break;
-
-    }
+     }
 
     glutPostRedisplay();
 }
 
 static void resize (int w, int h)
 {
-   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
    glMatrixMode (GL_PROJECTION);
    glLoadIdentity ();
+   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
    glFrustum (-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
    glMatrixMode (GL_MODELVIEW);
 }
